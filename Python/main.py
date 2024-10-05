846c6b78af11dd27b72fd1299f097c4b9177de3e
Jonak <jonakp.thakuria@gmail.com>
2024-10-05 21:12:08 +0530

    console error

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,7 @@
 # import sqlConnection as scon
 import pyscript as py
 from pyscript import document 
-
+from pyscript import console
 def addevent(event):
     # tf.create_event(event_name = document.getElementById('ev-name-box').text)
-    py.console.log( document.getElementById('ev-name-box').text)
+    console.log( document.getElementById('ev-name-box').text)
