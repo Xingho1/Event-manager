4ce23056fc7b06afa8791da7e65c6aa0aa57b0c1
Jonak <jonakp.thakuria@gmail.com>
2024-10-05 23:03:46 +0530

    test6

--- Python/main.py
+++ Python/main.py
@@ -4,5 +4,5 @@ from pyscript import document
 def addevent(event):
     # tf.create_event(event_name = document.getElementById('ev-name-box').text)
     
-    intxt = document.querySelector("#ev-name-box").innerHTML
+    intxt = document.querySelector("#ev-name-box").value
     document.querySelector(".ev-name").innerHTML = intxt
