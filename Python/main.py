495f8d3775c07468aafd4ae7a02c700f92f8e715
Jonak <jonakp.thakuria@gmail.com>
2024-10-05 21:28:36 +0530

    console err

--- Python/main.py
+++ Python/main.py
@@ -4,4 +4,6 @@ from pyscript import document
 from pyscript import console
 def addevent(event):
     # tf.create_event(event_name = document.getElementById('ev-name-box').text)
-    console.log( document.getElementById('ev-name-box').text)
+    out = document.getElementById('ev-name')
+
+    out.innerHTML = document.getElementById('ev-name-box').innerHTML
