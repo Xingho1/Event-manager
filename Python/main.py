407035e97c253488d8f2400f433f2323b2f10980
Jonak <jonakp.thakuria@gmail.com>
2024-10-05 20:54:03 +0530

    pyscript trial

--- Python/main.py
+++ Python/main.py
@@ -1,9 +1,7 @@
 # import sqlConnection as scon
-import pyscript
+import pyscript as py
 from pyscript import document 
-import firebase_admin as fire 
-import testfile as tf
 
 def addevent():
     # tf.create_event(event_name = document.getElementById('ev-name-box').text)
-    console.log( document.getElementById('ev-name-box').text)
+    py.console.log( document.getElementById('ev-name-box').text)
